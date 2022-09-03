5980282ed28dd1613393d4b4354c2a644bd6183e
Ian Waddington <iwaddo@icloud.com>
2022-09-03 18:12:24 +0100

    2.3 - Second Exercise

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var lowerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("👍 ViewDidLoad has Run!")
+        topLabel.text = ""
+        lowerLabel.text = ""
+
     }
 
+    @IBAction func topButtonPressed(_ sender: UIButton) {
+        print("The top button was pressed!")
+        topLabel.text = "Larry Page"
+        lowerLabel.text = "Sergey Brin"
+    }
+    
+    @IBAction func lowerButtonPressed(_ sender: UIButton) {
+        print("The lower button was pressed!")
+        topLabel.text = "Jennifer Hyman"
+        lowerLabel.text = "Jenny Fleiss"
 
+
+    }
 }
 
